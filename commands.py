0e8d2c62e9dd975ba2bb752d860dad5a71042767
Ankit Rathee <ankitrathee914@gmail.com>
2024-10-26 18:17:56 +0530

    added aCommmands.py

---
+++ commands.py
@@ -0,0 +1,7 @@
+# git push -u origin main    (to set upstream, in future if we will write only git push)
+# git checkout -b feature1 (To create new branch with nsme feature1)
+# git checkout branch name (To Navigate)
+# git branch -d branch name (To delete branch)
+# to delete any branch we should be in other branch otherwise error throw hoga
+
+ 
